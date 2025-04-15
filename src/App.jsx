4a1f449537b3ec98b966376e868322102d9f2bf1
Javohir708 <joxauzking@gamil.com>
2025-04-15 09:35:08 +0500

    about page qo'shildi

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './pages/layout/Layout'
 import Home from './pages/home/Home'
+import Collection from './pages/collection/Collection'
+import About from './pages/about/About'
+import Contact from './pages/contact/Contact'
 
 function App() {
 
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<Home />}/>
+          <Route path='/collection' element={<Collection />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />}/>
         </Route>
       </Routes>
     </>
