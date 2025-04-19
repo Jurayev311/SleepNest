cb469409b8d87691385be458d226d250187312d3
Javohir708 <joxauzking@gamil.com>
2025-04-19 19:55:05 +0500

    tayyor

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import WinterCollection from '../../components/winterCollection/WinterCollection'
+import ScrollToTop from '../../components/scroll/ScrollTop'
 
 const Collection = () => {
   return (
+    <>
+    <ScrollToTop />
     <section className='pt-[10px] mb-[20px]'>
       <div className='container mx-auto'>
         <WinterCollection />
       </div>
     </section>
+    </>
   )
 }
 
